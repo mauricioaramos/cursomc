f2524899761a335ab227aeb65721bd88aa419496
Mauricio Ramos <mauramosaraujo@gmail.com>
2021-04-25 20:22:37 -0300

    Estado e cidade

--- src/main/java/com/mauricao/cursomc/CursomcApplication.java
+++ src/main/java/com/mauricao/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mauricao.cursomc.domain.Categoria;
+import com.mauricao.cursomc.domain.Cidade;
+import com.mauricao.cursomc.domain.Estado;
 import com.mauricao.cursomc.domain.Produto;
 import com.mauricao.cursomc.repositories.CategoriaRepository;
+import com.mauricao.cursomc.repositories.CidadeRepository;
+import com.mauricao.cursomc.repositories.EstadoRepository;
 import com.mauricao.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +21,12 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,6 +52,19 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado estadoMinasGerais = new Estado(null, "Minas Gerais");
+		Estado estadoSaoPaulo = new Estado(null, "Sao paulo");
+		
+		Cidade cidadeUderlandia = new Cidade(null,"Uberlandia", estadoMinasGerais);
+		Cidade cidadeSaoPaulo = new Cidade(null,"Sao Paulo", estadoSaoPaulo);
+		Cidade cidadeCampinas = new Cidade(null,"Campinas", estadoSaoPaulo);
+		
+		estadoMinasGerais.getCidades().addAll(Arrays.asList(cidadeUderlandia));
+		estadoSaoPaulo.getCidades().addAll(Arrays.asList(cidadeSaoPaulo, cidadeCampinas));
+		
+		estadoRepository.saveAll(Arrays.asList(estadoMinasGerais, estadoSaoPaulo));
+		cidadeRepository.saveAll(Arrays.asList(cidadeUderlandia, cidadeSaoPaulo, cidadeCampinas));
 		
 	}
 
